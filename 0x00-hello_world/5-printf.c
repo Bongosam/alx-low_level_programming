d4a76da4f0efc9c07075c46f2736057b50f8dd5e
Sampson Bongotongo <sampsonbongotongo@gmail.com>
2023-02-16 15:33:36 -0800

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A programe to print using printf function
- * Return: 0(success)
+ * main - A programe to printa line  using printf function
+ * Return: 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
