2f86c1a21bde5cd52065b911158a02877726bf3b
Sampson Bongotongo <sampsonbongotongo@gmail.com>
2023-02-16 17:10:49 -0800

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - a c program that prints a line into the standard error
  * Return: 1(Success)
  */
 int main(void)
 {
-	fprintf(standerr, "and that piece of art is");
-	fprintf(standerr, "useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "and that piece of art is ");
+	fprintf(stderr, "useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
