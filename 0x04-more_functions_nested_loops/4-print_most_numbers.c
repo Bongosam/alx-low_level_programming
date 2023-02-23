320b6271673b4a026182fb9c0eed42f39f8d185d
Sampson Bongotongo <sampsonbongotongo@gmail.com>
2023-02-23 15:02:27 -0800

    new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,23 +3,17 @@
 /**
  * print_most_numbers - print numbers since 0 to 9
  * Description: print excluding the numbers 2 and 4
- * Return: the numbers since 0 up to 9
+ *
  */
 
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for (; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
+		if (x != 2 && x != 4)
+			_putchar((x % 10) + '0');
 	}
 	_putchar('\n');
 }
