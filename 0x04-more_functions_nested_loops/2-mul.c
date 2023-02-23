39e7fa82a0ff2ee065bd483a375ce553dc811a6e
Sampson Bongotongo <sampsonbongotongo@gmail.com>
2023-02-23 08:28:52 -0800

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of two numbers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ * Return: results from the operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
