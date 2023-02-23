3f03ebb596563881c60c76f3b5dd8cef580ace24
Sampson Bongotongo <sampsonbongotongo@gmail.com>
2023-02-23 08:15:21 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase
+ * @c: number to be checked
+ * Return: 1 for uppercase or 0 for otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
